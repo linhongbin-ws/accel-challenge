1f430e9e8a476c73ca9a4777ce06b96abe7f1677
Ben <522181941@qq.com>
2022-05-22 11:53:02 +0800

    fine tune param

--- accel_challenge/challenge2/param.py
+++ accel_challenge/challenge2/param.py
@@ -7,23 +7,26 @@ import numpy as np
 # grasp_point_offset = Frame(Rotation.RPY(*[0, 0, 0]), Vector(*[0,0,0])) # grasping point offset w.r.t. tool frame
 # grasp_point_offset = Frame(Rotation.RPY(*[0, 0, 0]), Vector(*[-0.02, 0,-0.058]))
 # grasp_point_offset = Frame(Rotation.RPY(*[0, 0, 0]), Vector(*[-0.02, 0,0.058]))
-grasp_point_offset = Frame(Rotation.RPY(*[0, 0, 0]), Vector(*[-0.01, 0,0.058]))
+# grasp_point_offset = Frame(Rotation.RPY(*[0, 0, 0]), Vector(*[-0.01, 0,0.05]))
+grasp_point_offset = Frame(Rotation.RPY(*[0, 0, 0]), Vector(*[0, 0, 0.09]))
 # grasp_point_offset = Frame(Rotation.RPY(*[0, 0, 0]), Vector(*[-0.02, 0.03,-0.058]))
 # grasp_point_offset = Frame(Rotation.RPY(*[0, 0, 0]), Vector(*[0,0,-0]))
 
 #==== grasp needle task
 
  # grasp target point offset w.r.t. needle frame
-T_gt_n = Frame(Rotation.RPY(*[0, 0, 0]), Vector(*[-0.09,0.03,0.]))
+# T_gt_n = Frame(Rotation.RPY(*[0, 0, 0]), Vector(*[-0.09,0.03,0.]))
+T_gt_n = Frame(Rotation.RPY(*[0, 0, 0]), Vector(*[-0.1,0.03,0.]))
+# T_gt_n = Frame(Rotation.RPY(*[0, 0, 0]), Vector(*[-0.05,0.08,0.]))
 T_hover_gt = RPY2T(0,0,0.3, 0, 0, 0)
 
 
 #==== suture task
-NEEDLE_R = 0.106 # needle radius
-NEEDLE_R = 0.103 # needle radius
+NEEDLE_R = 0.13 # needle radius
+# NEEDLE_R = 0.103 # needle radius
 
-# T_tip_n = Frame(Rotation.RPY(*[0, 0, 0]), Vector(*[0.055,0.083,0])) # tip frame w.r.t. needle base frame 0.04,0.07,0.
-T_tip_n = Frame(Rotation.RPY(*[0, 0, 0]), Vector(*[0.04,0.083,0])) # tip frame w.r.t. needle base frame
+T_tip_n = Frame(Rotation.RPY(*[0, 0, 0]), Vector(*[0.055,0.083,0])) # tip frame w.r.t. needle base frame 0.04,0.07,0.
+# T_tip_n = Frame(Rotation.RPY(*[0, 0, 0]), Vector(*[0.04,0.08/3,0])) # tip frame w.r.t. needle base frame
 
 
 
